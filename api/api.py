23d3aa59a9220b53a6c19b35ba84ac320914ba5f
Jackie <kennedy.lsd32@gmail.com>
2025-03-01 22:39:58 +0100

    add new endpoints and save prices to the database

--- api/api.py
+++ api/api.py
@@ -1,41 +1,49 @@
-
-from fastapi import BackgroundTasks, FastAPI
+from fastapi import BackgroundTasks, Depends, FastAPI
 from fastapi.concurrency import asynccontextmanager
-
-from models.db import check_connection, close_db
+from sqlalchemy.ext.asyncio import AsyncSession    
 from schemas.schemas import RequestData
 from .scrapper import Scrapper
+from models.db import DbManager
+import logging
+
+database_manager = DbManager()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("Statring up...")
-    await check_connection()
-    print("DB connection is successful!")
+    logger.info("Starting up...")
+    await database_manager.check_connection()
+    await database_manager.create_tables()
     yield
-    print("Shutdown..")
-    await close_db()
+    logger.info("Shutdown..")
+    await database_manager.close_connection()
 
 app = FastAPI(lifespan=lifespan)
 
-def scrape_and_send_email(browser: str, url: str):
-    try:
-        scrap = Scrapper()
-        scrap.set_driver(browser, url)
-        data = scrap.check_original_price(2)
-        scrap.send_email(data[0], data[1])
-    except Exception as e:
-        print(f"Error during scraping or email sending: {e}")
+async def scrape_and_send_email(browser: str, url: str):
+    async with database_manager.get_db() as session:
+        try:
+            scrap = Scrapper()
+            scrap.set_driver(browser, url)
+            data = scrap.check_original_price(2)
+            await database_manager.save_price(data, session)
+            scrap.send_email(data)
+        except Exception as e:
+            logger.error(f"Error during scraping or email sending: {e}")
 
-
-@app.post("/show")
+@app.post("/scrape")
 async def scrape_price(data_for_request: RequestData, background_tasks: BackgroundTasks):
     browser = data_for_request.browser
     url = data_for_request.url
     
     background_tasks.add_task(scrape_and_send_email, browser, url)
-
     return {"status": "Processing", "message": "Scraping and email sending started."}
 
+@app.get("/show")
+async def show_prices(session: AsyncSession = Depends(database_manager.get_db)):
+    cars = await database_manager.show_prices(session)
 
-
+    return cars
 
